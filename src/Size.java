5a85c85052299a3782ba34bb2372c95c34736cbd
AfonsoSemeano <afonso.semeano20@estudantes.ips.pt>
2021-03-04 11:44:02 +0000

    JavaDoc for Size

--- src/Size.java
+++ src/Size.java
@@ -9,6 +9,13 @@ public enum Size {
     private final int maxValue;
     private final char code;
 
+    /**
+     *
+     * @param minValue Valor mínimo do tamanho
+     * @param maxValue Valor máximo do tamanho
+     * @param description Descrição do tamanho
+     * @param code Código de um único caracter do tamanho
+     */
     private Size(int minValue, int maxValue, String description, char code){
         this.description = description;
         this.minValue = minValue;
@@ -16,23 +23,39 @@ public enum Size {
         this.code = code;
     }
 
+    /**
+     * Representa o objeto
+     * @return Descrição do objeto
+     */
     @Override
     public String toString(){
         return this.description;
     }
 
+    /**
+     * @return Descrição do tamanho
+     */
     public String getDescription(){
         return description;
     }
 
+    /**
+     * @return Valor mínimo
+     */
     public int getMinValue(){
         return minValue;
     }
 
+    /**
+     * @return Valor máximo
+     */
     public int getMaxValue(){
         return maxValue;
     }
 
+    /**
+     * @return Caracter único do tamanho
+     */
     public char getCode(){
         return code;
     }
