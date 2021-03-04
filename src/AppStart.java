c3d892fb0c6d344651ff94627930a5dd70d514f5
AfonsoSemeano <afonso.semeano20@estudantes.ips.pt>
2021-03-04 11:35:34 +0000

    New AppStart

--- src/AppStart.java
+++ src/AppStart.java
@@ -1,6 +1,18 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class AppStart{
     public static void main(String [] args){
-        System.out.println("Hello, welcome");
-        System.out.println("Hope you enjoy your stay");
+        Scanner inputReader = new Scanner(System.in);
+        System.out.println("Escreva um tamanho (Small, Medium ou Large).");
+        String input = inputReader.next();
+        input = input.toUpperCase();
+        Size size = Size.valueOf(input);
+        System.out.println("Name:           "+input);
+        System.out.println("toString():     "+size.toString());
+        System.out.println("Ordem:          "+size.ordinal());
+        System.out.println("Valor minimo:   "+size.getMinValue());
+        System.out.println("Valor maximo:   "+size.getMaxValue());
+        System.out.println("Código:         "+size.getCode());
     }
 }
