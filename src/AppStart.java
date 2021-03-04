d9c2e225de2e8bfd04974a6af1d52fd893e8559d
AfonsoSemeano <afonso.semeano20@estudantes.ips.pt>
2021-03-04 10:45:51 +0000

    Nothing much

--- src/AppStart.java
+++ src/AppStart.java
@@ -1,5 +1,6 @@
-public class Project {
+public class AppStart{
     public static void main(String [] args){
         System.out.println("Hello, welcome");
+        System.out.println("Hope you enjoy your stay");
     }
 }
